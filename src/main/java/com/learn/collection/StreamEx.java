9d90b1a9e4ae9101ec14a548c24ff8ff5ec2cf9e
midoshkm@hotmail.com <magee2020>
2023-02-26 21:45:31 +0530

    updated Streams Examples

--- src/main/java/com/learn/collection/StreamEx.java
+++ src/main/java/com/learn/collection/StreamEx.java
@@ -1,20 +1,59 @@
 package com.learn.collection;
 
-import java.util.OptionalDouble;
-import java.util.Random;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-
-//mentee
-// knowing the path
-// walking the path
-
-//mentor
-// show the path
-
 public class StreamEx {
+
     public static void main(String[] args) {
+
+        List<Student> student = new ArrayList<>();
+        student.add(Student.builder().name("malu").age(8).city("chennai").subject("english").build());
+        student.add(Student.builder().name("adhu").age(8).city("ernakulam").subject("maths").build());
+        student.add(Student.builder().name("samrudh").age(8).city("ernakulam").subject("malayalam").build());
+        student.add(Student.builder().name("sharan").age(12).city("chennai").subject("tamil").build());
+        student.add(Student.builder().name("pooja").age(8).city("chennai").subject("tamil").build());
+
+        // group by attribute name
+        Map<String, List<Student>> subjectMap =
+                student.stream()
+                        .collect(Collectors.groupingBy(Student::getSubject));
+
+        subjectMap.forEach((key,value)-> System.out.println( " subject  : " + key +" \n students "+ value));
+
+        // group by attribute name and down stream collector
+        Map<String, List<String>> studentMap =
+                student.stream()
+                        .collect(Collectors.groupingBy(Student::getCity,
+                                Collectors.mapping(Student::getName, Collectors.toList())));
+
+        studentMap.forEach((key,value)-> System.out.println( " city  : " + key +" \n student names "+ value));
+
+        // group by attribute name, count for each group
+        Map<String, Long> studentCountMap =
+                student.stream()
+                        .collect(Collectors.groupingBy(Student::getCity, Collectors.counting()));
+
+        studentCountMap.forEach((key,value)-> System.out.println( " city  : " + key +" \n no of students "+ value));
+
+        // group by attribute name, hashmap implementation, downstream collector
+        Map<String, List<String>> studentTreeMap =
+                student.stream()
+                        .collect(Collectors.groupingBy(Student::getCity,
+                                TreeMap::new,
+                                Collectors.mapping(Student::getName, Collectors.toList())));
+
+        studentTreeMap.forEach((key,value)-> System.out.println( " city  : " + key +" \n student names "+ value));
+
+
+    }
+    public static void streamGenerateIterateOf() {
 
         Stream.generate(new Random()::nextInt)
                 .filter(i -> i>0)
@@ -48,4 +87,15 @@ public class StreamEx {
 
 
     }
+}
+
+@Builder
+@Data
+@ToString
+class Student {
+    String name;
+    Integer age;
+    String city;
+    String subject;
+
 }
