71638dec33dde44fc229229a6759e428f43db93c
midoshkm@hotmail.com <magee2020>
2023-02-26 11:35:36 +0530

    added Map and Collections

--- src/main/java/com/learn/collection/CollectionOperations.java
+++ src/main/java/com/learn/collection/CollectionOperations.java
@@ -14,5 +14,19 @@ public class CollectionOperations {
         //Collections.sort(list, Comparator.comparing(String::length)); // custom Order
         list.forEach(System.out::println);
 
+        int index = Collections.binarySearch(list, "ef");
+        System.out.println(" ef is present at location "+ index);
+
+        Collections.shuffle(list);
+
+        System.out.println(list);
+
+        int index2 = Collections.binarySearch(list, "fg");
+        System.out.println(" after shuffling, fg is present at location "+ index2);
+
+        Collections.reverse(list);
+        System.out.println(" after reversing list "+ list);
+
+
     }
 }
